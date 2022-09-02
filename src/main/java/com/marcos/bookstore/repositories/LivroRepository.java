d60521b1279bdc165a3bbcb557c55ab6a97fa049
marcosduarte-dev <pe.marcos30@gmail.com>
2022-09-02 15:48:33 -0300

    Instancia automatica da base de dados

--- src/main/java/com/marcos/bookstore/repositories/LivroRepository.java
+++ src/main/java/com/marcos/bookstore/repositories/LivroRepository.java
@@ -3,9 +3,9 @@ package com.marcos.bookstore.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.marcos.bookstore.domain.Categoria;
+import com.marcos.bookstore.domain.Livro;
 
 @Repository
-public interface LivroRepository extends JpaRepository<Categoria, Integer>{
+public interface LivroRepository extends JpaRepository<Livro, Integer>{
 
 }
